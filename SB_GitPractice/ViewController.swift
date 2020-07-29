bb7fff832b176abd50b34598ed0ec4f3dd177310
Hiromichi Kitamura <Hiromichi.Kitamura@ibm.com>
2020-07-29 13:47:51 +0900

    Initial Commit

---
+++ SB_GitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SB_GitPractice
+//
+//  Created by aha05602 on 2020/07/29.
+//  Copyright © 2020 Hiromichi Kitamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
